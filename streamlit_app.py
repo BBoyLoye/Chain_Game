b1590df30f13b1714392dd4d929d8e1c1cdca7ff
Brandon Loye <b.loye.17@gmail.com>
2026-09-08 16:25:26 -0500

    Added instructions

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,22 @@ from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title("Come Play the Chain Game!")
 st.write(
-  """Replace this example with your own code!
-  **And if you're new to Streamlit,** check
-  out our easy-to-follow guides at
-  [docs.streamlit.io](https://docs.streamlit.io).
+  """Welcome to my project! If it's your first time playing, please click the button below.
   """
 )
+
+with st.expander("It's my first time"):
+    st.write("My name is Brandon. I designed this game primarily as a portfolio piece to showcase my skills in data engineering and data science.")
+    st.write("I currently work as a solutions engineer at Motive. I am proud to sell life-saving IOT devices and cutting-edge software, but since we've started offering some data analytics products and services, I've decided that my true passion is building solutions around these sorts of products.")
+    st.write("The Chain Game is a simple simulation of a supply chain network. It is a game where you are a supply chain manager and you need to decide which routes to take to transport goods from your company's factory in Shenzhen, to your dozen stores in the US.")
+    st.write("As a supply chain manager, you have two tools at your disposal:")
+    st.write("    1. You can make contracts with freight brokers to transport goods. You should make contracts to save money over paying 'spot rates,' which are often higher. But beware! You risk contracts being bloated, and forcing you to pay for freight services you won't need if demand goes down.")
+    st.write("    2. Once your contracts are locked in, you'll be able to navigate REAL TIME spot rates for ocean, rail, and truck cargo to get products where they belong")
+    st.write("")
+    st.write("Your goal is to get products to where they belong for as little cost as possible. Good luck!")
+    
+# You can also add formatting, bold text, or markdown links inside
+st.markdown("**Tip:** Check the sidebar for current freight market rates!")
 
 cnx = st.connection("snowflake")
 session = cnx.session()
